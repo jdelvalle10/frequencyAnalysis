79d6215aa046f8bcb0c01dcbe87066a727f28cdf
Jose Del Valle <36140117+jdelvalle10@users.noreply.github.com>
2024-04-12 13:56:55 -0400

    Update freqAnalysis.py

--- freqAnalysis.py
+++ freqAnalysis.py
@@ -1,16 +1,31 @@
-def read_file(ciphered):
-    with open(ciphered, 'r', ) as file:
-            ciphered_text = file.read()
-    return ciphered_text
+# Script that takes a text file and calculates the frequency of each character in the text.
+
+def openfile(cipher):
+    file = open(cipher, 'r')
+    cipheredtext = file.read()
+    file.close()
+    return cipheredtext
+
+def uppercasing(cipher):
+    cipherUp = ""
+    for c in cipher:
+        if c.isalpha():
+            cipherUp += c.upper()
+
+    return cipherUp
+
+def frequencies(cipheredtext):
+    freqDict = {}
+    for c in cipheredtext:
+        if not c in freqDict.keys():
+            freqDict[c] = cipheredtext.count(c)
+    return freqDict
 
 
-def frequencyAnalysis(cipheredText):
-    frequency = {}
-    for letter in cipheredText:
-        frequency[letter] = (cipheredText.count(letter)/len(cipheredText))*100
-    return frequency
+a = openfile("text.txt")
+b = uppercasing(a)
+table = frequencies(b)
 
-cipher = read_file('cipherText.txt')
-possibleKey = frequencyAnalysis(cipher)
-
+for key in table:
+    print(f"{key} -> {table[key]/len(table):.2f}%")
 
